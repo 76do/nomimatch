b4849de42f6b6896efe531e7b6519d0bf6c19b58
76do <isshi0210@gmail.com>
2023-02-20 19:07:19 +0900

    Fix: メール体裁調整

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -3,6 +3,6 @@ class RequestMailer < ApplicationMailer
   def request_email
     @user = params[:user]
     @request = params[:request]
-    mail(to: @user.email, subject: "#{@user.name}さん宛に飲み会依頼が届きました！")
+    mail(to: @user.email, subject: "#{@request.sender_name}さんから飲み会依頼が届きました！")
   end
 end
