d6bd9da1c0fedbd978edf72b3778f65a589c88d7
76do <isshi0210@gmail.com>
2023-03-03 12:39:39 +0900

    Fix: 不要テーブル(request_histories)を削除した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_06_231747) do
+ActiveRecord::Schema.define(version: 2023_03_03_033753) do
 
   create_table "api_keys", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -20,12 +20,6 @@ ActiveRecord::Schema.define(version: 2023_02_06_231747) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "active"
     t.index ["user_id"], name: "index_api_keys_on_user_id"
-  end
-
-  create_table "request_histories", charset: "utf8", force: :cascade do |t|
-    t.string "phone_number"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "requests", charset: "utf8", force: :cascade do |t|
