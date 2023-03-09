d60a04e97131cf97b83e761c79ef518c9b6f414a
76do <isshi0210@gmail.com>
2023-03-09 18:46:37 +0900

    Add: Authenticationsモデルにaccess_tokenカラムとaccess_token_secretカラムを追加

---
+++ db/migrate/20230309093717_add_access_token_and_access_token_secret.rb
@@ -0,0 +1,6 @@
+class AddAccessTokenAndAccessTokenSecret < ActiveRecord::Migration[6.1]
+  def change
+    add_column :authentications, :access_token, :string
+    add_column :authentications, :access_token_secret, :string
+  end
+end
