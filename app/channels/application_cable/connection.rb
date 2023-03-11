e356d7db999b206bf8074efaed85ab1e4111afc7
76do <isshi0210@gmail.com>
2023-03-12 01:05:06 +0900

    cors設定変更

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,7 +10,6 @@ module ApplicationCable
     protected
 
     def find_verified_user!
-      binding.irb
       user = ApiKey.active_token.find_by(access_token: cookies[:accessToken])&.user
       reject_unauthorized_connection unless user 
       return user
