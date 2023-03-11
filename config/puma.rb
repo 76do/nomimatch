8a635998a093b958dd03fd92252f6b3afc71b967
76do <isshi0210@gmail.com>
2023-03-11 22:04:51 +0900

    Fix: ActionCableの本番用設定を追加

--- config/puma.rb
+++ config/puma.rb
@@ -16,10 +16,10 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 # development環境の時は以下を使用
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # production環境の時は以下を使用
-# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
