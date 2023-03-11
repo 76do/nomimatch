30fc01bff0f5f1646022e690550a2bb250249df9
76do <isshi0210@gmail.com>
2023-03-12 00:29:42 +0900

    Fix: fixTypo

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -10,8 +10,7 @@ module ApplicationCable
     protected
 
     def find_verified_user!
-      puts params
-      puts cookies
+      binding.irb
       user = ApiKey.active_token.find_by(access_token: cookies[:accessToken])&.user
       reject_unauthorized_connection unless user 
       return user
